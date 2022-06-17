026abcd5101a95164013a5317012cf190ceb7cbd
RitzPriti <ritesh_243@yahoo.com>
2022-06-17 17:36:23 +0100

    first commit

--- src/test/java/org/example/RegistrationJUnit.java
+++ src/test/java/org/example/RegistrationJUnit.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationJUnit {
+    WebDriver driver;
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver =new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void OpnUrl(){
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.className("ico-register")).click();
+
+    }
+
 }
+
